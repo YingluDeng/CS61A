ba5d471618def92ea13815a0a8c5a1ce09e9863f
Yinglu (Cecilia) Deng <CECILIADENG12@GMAIL.COM>
2020-08-14 02:07:19 -0700

    Add files via upload

---
+++ lab/lab08/tests/wwpd-car.py
@@ -0,0 +1,74 @@
+test = {
+  'name': 'Car',
+  'points': 0,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> from car import *
+          >>> deneros_car = Car('Tesla', 'Model S')
+          >>> deneros_car.model
+          'Model S'
+          >>> deneros_car.gas = 10
+          >>> deneros_car.drive()
+          'Tesla Model S goes vroom!'
+          >>> deneros_car.drive()
+          'Cannot drive!'
+          >>> deneros_car.fill_gas()
+          'Gas level: 20'
+          >>> deneros_car.gas
+          20
+          >>> Car.gas
+          30
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> from car import *
+          >>> deneros_car = Car('Tesla', 'Model S')
+          >>> deneros_car.wheels = 2
+          >>> deneros_car.wheels
+          2
+          >>> Car.num_wheels
+          4
+          >>> deneros_car.drive() # Type Error if an error occurs and Nothing if nothing is displayed
+          'Cannot drive!'
+          >>> Car.drive() # Type Error if an error occurs and Nothing if nothing is displayed
+          Error
+          >>> Car.drive(deneros_car) # Type Error if an error occurs and Nothing if nothing is displayed
+          'Cannot drive!'
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> from car import *
+          >>> deneros_car = MonsterTruck('Monster', 'Batmobile')
+          >>> deneros_car.drive() # Type Error if an error occurs and Nothing if nothing is displayed
+          543d3c4044c7b885e6bc773187315cb9
+          2e96bc0878a8a5a4b77deec4ef4b3d09
+          # locked
+          >>> Car.drive(deneros_car) # Type Error if an error occurs and Nothing if nothing is displayed
+          2e96bc0878a8a5a4b77deec4ef4b3d09
+          # locked
+          >>> MonsterTruck.drive(deneros_car) # Type Error if an error occurs and Nothing if nothing is displayed
+          543d3c4044c7b885e6bc773187315cb9
+          2e96bc0878a8a5a4b77deec4ef4b3d09
+          # locked
+          >>> Car.rev(deneros_car) # Type Error if an error occurs and Nothing if nothing is displayed
+          8a2fd4e4c3c6dcce2dc631af62ee217a
+          # locked
+          """,
+          'hidden': False,
+          'locked': True
+        }
+      ],
+      'scored': False,
+      'type': 'wwpp'
+    }
+  ]
+}
